26ac6fb38a5f4a6093080944a9f1bf26c79b46f2
Lam Pham <lam.pham3110@gmail.com>
2018-07-02 10:00:52 -0400

    test

--- Fridge101/app/src/main/java/edu/wit/mobileapp/fridge101/MainActivity.java
+++ Fridge101/app/src/main/java/edu/wit/mobileapp/fridge101/MainActivity.java
@@ -1,7 +1,8 @@
 package edu.wit.mobileapp.fridge101;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v("Testing", "Okay");
     }
 }
